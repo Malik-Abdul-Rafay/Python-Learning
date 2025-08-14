9ef9f20a8678a0c788ef91ed2d4daf0306bb1350
rootXploit <rootxploit@kali.com>
2025-08-14 11:16:26 -0400

    main

---
+++ login_system.py
@@ -0,0 +1,7 @@
+users = {"Rafay" : {"password" : "rafay2005"}, "ahmed" :  {"password" : "ahmed2005"}, "basit" : {"password" : "basit2005"},}
+
+user_name = input("Enter User Name")
+user_password = input("Enter User Password")
+
+for user in users:
+    print(user)
